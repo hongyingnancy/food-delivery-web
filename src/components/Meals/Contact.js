bb8a86215140cd853879ba00c42587c9e0ddd1b7
yizhengshi <85378870+yizhengshi@users.noreply.github.com>
2022-01-23 12:03:37 -0600

    change contact info

--- src/components/Meals/Contact.js
+++ src/components/Meals/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
       <p>
         You can find me by 
       </p>
-      <GithubOutlined onClick={() =>  window.location.href='https://github.com/syz58'} style={{ fontSize: '300%'}}/>&nbsp;&nbsp;&nbsp;&nbsp;
+      <GithubOutlined onClick={() =>  window.location.href='https://github.com/yizhengshi'} style={{ fontSize: '300%'}}/>&nbsp;&nbsp;&nbsp;&nbsp;
       <LinkedinOutlined onClick={() =>  window.location.href='https://www.linkedin.com/in/yizhengshi'} style={{ fontSize: '300%'}}/>&nbsp;&nbsp;&nbsp;
       <TwitterOutlined onClick={() =>  window.location.href='https://www.twitter.com/YS58888'} style={{ fontSize: '300%'}}/><br></br>
       <br></br>
