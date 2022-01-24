b0f7290b13bf3076276a2ac01c3b6ac6cfb6aaf5
syz58 <85378870+syz58@users.noreply.github.com>
2022-01-23 22:20:15 -0600

    Modify Our Mission pagecontent

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -3,14 +3,14 @@ import classes from './MealsSummary.module.css';
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
-      <h2>We delivery the best food for you!</h2>
+      <h2>We provide the best Asian food for you!</h2>
       <p>
-        We provide Asian foods including Chinese, Japanese, Korean, and Tailand. You can view them on
-        menu. 
+        We provide Asian foods including Chinese, Japanese, Korean, and Tailand.
       </p>
       <p>
         Our mission is to provide the best Asian food in Wisconsin, Madison within 30 mins. 
       </p>
+      <h2> View more on menu page</h2>
     </section>
   );
 };
